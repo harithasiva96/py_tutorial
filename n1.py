4a8e1985bedc81eb3ab818dbf2567c79a310b92c
haritha <harithasiva007@gmail.com>
2025-02-18 10:23:17 +0530

    first python code

---
+++ n1.py
@@ -0,0 +1,27 @@
+# x=y=z="Orange"
+# print(x)
+# print(y)
+# print(z)
+
+# x,y,z = "Apple", "Orange","Grapes"
+# print(x)
+# print(y)
+# print(z)
+
+# F=["fig","Blueberry","kiwi"]
+# a,b,c=F
+# print(a)
+# print(b)
+# print(c)
+
+# x='python'
+# y='is'
+# z='awesome'
+# print(x,y,z)
+# print(x+y+z)
+
+x=5
+y="john"
+# print(x+y)
+print(x,y)
+
